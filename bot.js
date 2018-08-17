ee9d2fe144be9eb720a8f05ed5c15e41fbe9d30f
JPlexer <32966779+JPlexer@users.noreply.github.com>
2018-08-17 22:53:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ const functions = require("./modules/functions.js")
 client.on('ready', () => {
   console.log('Ready!')
   console.log(functions.setGame)
-  client.setInterval(functions.setGame, 30000);
+  client.setInterval(functions.setGame(), 30000);
   functions.setGame();
   clbot.setNick(`${branch}`);
 });
