59e8bc20822c93f6d9e1858ea2b7f471a00f4953
JPlexer <32966779+JPlexer@users.noreply.github.com>
2018-08-19 19:47:41 +0200

    Update useful.js

--- modules/useful.js
+++ modules/useful.js
@@ -13,7 +13,7 @@ module.exports = {
             type: "PLAYING"
         });
     },
-    ping:function ('message', message){ 
+    ping: function () {
         message.channel.send(`:ping_pong:Pong! Your ping is \`${`${Date.now() - message.createdTimestamp}`} ms\``);
-},
+    },
 }
