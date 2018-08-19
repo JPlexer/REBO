b506842f97dc682dcd4b8e96d919f180d38e9732
JPlexer <32966779+JPlexer@users.noreply.github.com>
2018-08-19 18:53:57 +0200

    Update useful.js

--- modules/useful.js
+++ modules/useful.js
@@ -1,17 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const functions = require("./functions.js")
-client.login(process.env.BOT_TOKEN);
 module.exports = {
     setGame: function () {
-        client.user.setActivity(functions.getRandom(
-            "with my Users",
-            "Annoying JPlexer",
-            `${prefix}help`,
-            `${botver}`,
-            `${botver}`,
-            `${prefix}help`), {
-            type: "PLAYING"
-        });
-    },
+        client.on('ready', () => {
+            client.user.setActivity(functions.getRandom(
+                "with my Users",
+                "Annoying JPlexer",
+                `${prefix}help`,
+                `${botver}`,
+                `${botver}`,
+                `${prefix}help`), {
+                type: "PLAYING"
+            });
+        })
+    }
 }
