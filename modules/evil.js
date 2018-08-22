b2cd5a5a2a7a9a303a350fa80e6578856a0e40fa
JPlexer <32966779+JPlexer@users.noreply.github.com>
2018-08-22 21:32:01 +0200

    Add files via upload

--- modules/evil.js
+++ modules/evil.js
@@ -1,24 +1,42 @@
-module.exports = {
-    clean: function(){ text => {
-        if (typeof(text) === "string")
-          return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-        else
-            return text;
+const Discord = require("discord.js");
+
+module.exports.run = async (client,message,args) => {
+    if (message.member !== 348065394520621067)return message.channel.send("**Hey, `" + message.author.username + "` You Can't use eval only JPlexer can!**");
+
+const clean = text => {
+  if (typeof(text) === "string")
+    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+  else
+      return text;
+}
+
+    try{
+      const code = args2.join(" ");
+      let evaled = eval(code);
+
+      if (typeof evaled !== "string"){
+        evaled = require("util").inspect(evaled);
+      }
+
+    if (evaled.includes(client.token)){
+        evaled = evaled.replace(client.token, "THATS MY PRIVESSEESEES NO! AND I MEAN NO! \n\ YOU GET I WILL CALL THE COPS 👮");
+
+        if (evaled.includes(client.token)){
+            evaled = evaled.replace(client.token, "THATS MY PRIVESSEESEES NO! AND I MEAN NO! \n\ YOU GET I WILL CALL THE COPS 👮");
+        }   
     }
-      },
-      args: function(message){
-        message.content.split(" ").slice(1);
-      },
-        eval: function(message) {
-            if(message.author.id !== 348065394520621067) return;
-            try {
-              const code = args.join(" ");
-              let evaled = eval(code);
-              if (typeof evaled !== "string")
-                evaled = require("util").inspect(evaled);
-              message.channel.send(this.clean(evaled), {code:"xl"});
-            } catch (err) {
-              message.channel.send(`\`ERROR\` \`\`\`xl\n${this.clean(err)}\n\`\`\``);
-            }
-},
+
+    let embed = new Discord.RichEmbed()
+    .addField(`Input`, "***```" + code + "```***", true)
+    .addField(`Output`, `\`\`\`python\n${evaled}\`\`\``,)
+    .setColor("#cb36ed")//
+    .setTimestamp()
+
+
+
+message.channel.send(embed).then(msg => {msg.delete(25000)});
+
+    } catch (err) {
+    message.channel.send('``Error``\n ```'+ clean(err) +'```').then(msg => {msg.delete(25000)});
+    }    
 }
